fff0c880ca8d3c8efd8659c53e3b516f974c58b9
remzmike <remzmike@users.noreply.github.com>
2016-09-21 15:05:59 -0400

    .

---
+++ Library.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace cchtask
+{
+    class Library
+    {
+        // practical
+        private DataTable resourceData;
+        private DataTable availabilityData;
+
+        public Library()
+        {
+            resourceData = new DataTable("resource");
+            availabilityData = new DataTable("availability");
+        }
+
+        private void Load()
+        {
+            resourceData.ReadXmlSchema("..\\resource.xsd");
+            resourceData.ReadXml("..\\resource.xml");
+
+            availabilityData.ReadXmlSchema("..\\availability.xsd");
+            availabilityData.ReadXml("..\\availability.xml");
+        }
+
+        private void Save()
+        {
+            availabilityData.WriteXml("..\\availability.xml");
+        }
+
+        public void List(string sortBy)
+        {
+            int id;
+            bool availability;
+            DataRow row;
+
+            Load();
+            resourceData.DefaultView.Sort = sortBy;
+
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("id | title           | author             | publisher       | date | available?");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            foreach (DataRowView rowView in resourceData.DefaultView)
+            {
+                row = rowView.Row;
+                id = row.Field<int>("id");
+                availability = availabilityData.Rows[id].Field<bool>("available");
+                Console.WriteLine("{0,2} | {1,-15} | {2,-18} | {3,-15} | {4:yyyy} | {5}", row["id"], row["title"], row["author"], row["publisher"], row["date"], availability);
+            }
+        }
+
+        public void List()
+        {
+            List("id");
+        }
+
+        public void CheckIn(int id)
+        {
+            Load();
+            availabilityData.Rows[id]["available"] = true;
+            Save();
+        }
+
+        public void CheckOut(int id)
+        {
+            Load();
+            availabilityData.Rows[id]["available"] = false;
+            Save();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cchtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Library library;
+
+            if (args.Length==0 || !args[0].StartsWith("-") )
+            {
+                Help();
+            }
+            else
+            {
+                library = new Library();
+
+                try
+                {
+                    switch (args[0])
+                    {
+                        case "-list":
+                            library.List();
+                            break;
+                        case "-listsortby":
+                            library.List(GetArgument(args));
+                            break;
+                        case "-checkout":
+                            library.CheckOut(GetIntArgument(args));
+                            break;
+                        case "-checkin":
+                            library.CheckIn(GetIntArgument(args));
+                            break;
+                        case "-?":
+                            Help();
+                            break;
+                        default:
+                            Help();
+                            break;
+                    }
+                }
+                catch (CommandLineException)
+                {
+                    Help();
+                }
+            }
+        }
+
+        static void Help()
+        {
+            string helpString = @"
+Usage: cchtask -COMMAND [ARGUMENT]
+
+Commands:
+
+    -list
+    -listsortby [column]
+    -checkout [id]
+    -checkin [id]
+
+Examples:
+
+    cchtask -list
+    cchtask -listsortby title
+    cchtask -listsortby ""title desc""
+    cchtask -checkout 1
+    cchtask -checkin 1
+";
+            Console.Write(helpString);
+        }
+
+        static string GetArgument(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                return args[1];
+            }
+            else
+            {
+                throw new CommandLineException();
+            }
+        }
+
+        static int GetIntArgument(string[] args)
+        {
+            int i;
+            string s = GetArgument(args);
+            if (Int32.TryParse(s, out i))
+            {
+                return i;
+            }
+            else
+            {
+                throw new CommandLineException();
+            }
+        }
+
+    }
+
+    class CommandLineException : Exception
+    {
+    }
+
+}
